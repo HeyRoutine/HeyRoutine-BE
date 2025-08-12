dedd9ce4295a3112339297b733d8697ab9ab55bf
zeus <jj99526@naver.com>
2025-08-13 02:16:36 +0900

    :sparkles: feat[#3]: 유저 회원가입 로그인 구현 - 회원가입 - 로그인 - 닉네임 중복확인 - 이메일 중복확인 - redis에 토큰저장 - 토큰 재발급 - 비밀번호 찾기 - 비밀번호 재설정 - 닉네임 재설정

---
+++ src/main/java/com/saeparam/HeyRoutine/user/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.saeparam.HeyRoutine.user.repository;
+import com.saeparam.HeyRoutine.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  Optional<User> findByEmail(String username);
+
+  boolean existsByEmail(String username);
+  boolean existsByNickname(String nickname);
+
+}
