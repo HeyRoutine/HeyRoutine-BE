ff0f823f6460ee868cb94c544c8ceddad04f01fb
Junho Yu <ryuwon@gnu.ac.kr>
2025-08-13 00:28:02 +0900

    🎉 Init[#1]: 초기 프로젝트 파일 등록

---
+++ src/main/java/com/saeparam/HeyRoutine/HeyRoutineApplication.java
@@ -0,0 +1,13 @@
+package com.saeparam.HeyRoutine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyRoutineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyRoutineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saeparam/HeyRoutine/HeyRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saeparam.HeyRoutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeyRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
