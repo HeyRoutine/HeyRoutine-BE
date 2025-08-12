8fbbf4c323a02100629ee168ff9bad7511f1b6e8
zeus <jj99526@naver.com>
2025-08-13 02:13:12 +0900

    :sparkles: feat[#1]: Exception,Response 설정 - 예외처리 형식화 - 반환값 형식화

---
+++ src/main/java/com/saeparam/HeyRoutine/exception/GeneralException.java
@@ -0,0 +1,23 @@
+package com.saeparam.HeyRoutine.exception;
+
+
+
+import com.saeparam.HeyRoutine.response.code.BaseErrorCode;
+import com.saeparam.HeyRoutine.response.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus() {
+        return this.code.getReasonHttpStatus();
+    }
+}
