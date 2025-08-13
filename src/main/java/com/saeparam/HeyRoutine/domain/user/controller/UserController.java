86912bc5c8b64e27ef5a0356978530fd38d1cb65
zeus <jj99526@naver.com>
2025-08-13 23:58:59 +0900

    :sparkles: feat[#2]: 이메일 인증 구현 - 이메일 인증 - 비밀번호 찾기시 UUID를 설정하여 API탈취시 보안문제 해결

--- src/main/java/com/saeparam/HeyRoutine/domain/user/controller/UserController.java
+++ src/main/java/com/saeparam/HeyRoutine/domain/user/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/user")
+@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
