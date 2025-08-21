ad0143388e6bd746d3338af93ccd5851652f5c9e
ryuwon <ryuwon@gnu.ac.kr>
2025-08-21 11:51:12 +0900

    ✨ Feat[#21]: GroupRoutineList Entity 추가

---
+++ src/main/java/com/saeparam/HeyRoutine/domain/routine/entity/GroupRoutineList.java
@@ -0,0 +1,44 @@
+package com.saeparam.HeyRoutine.domain.routine.entity;
+
+import com.saeparam.HeyRoutine.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import com.saeparam.HeyRoutine.domain.routine.enums.RoutineType;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroupRoutineList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_routine_list_id", updatable = false, unique = true, nullable = false)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "routine_type", nullable = false)
+    private RoutineType routineType;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 255)
+    private String description;
+
+    @Column(name = "startTime", nullable = false)
+    private LocalTime startTime;
+
+    @Column(name = "endTime", nullable = false)
+    private LocalTime endTime;
+
+    @Column(name = "userCnt", nullable = false)
+    private int userCnt;
+}
