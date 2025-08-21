f969ff47439664ede736de864a43bc8d3130d5f9
ryuwon <ryuwon@gnu.ac.kr>
2025-08-21 11:52:35 +0900

    ✨ Feat[#21]: Guestbook Entity 추가

---
+++ src/main/java/com/saeparam/HeyRoutine/domain/routine/entity/Guestbook.java
@@ -0,0 +1,28 @@
+package com.saeparam.HeyRoutine.domain.routine.entity;
+
+import com.saeparam.HeyRoutine.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Guestbook {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "guestbook_id", updatable = false, unique = true, nullable = false)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_routine_list_id", nullable = false)
+    private GroupRoutineList groupRoutineList;
+
+    @Column(name = "content", nullable = false, length = 255)
+    private String content;
+}
