60d30406b3a57c3295e602664749da73cb4e864d
ryuwon <ryuwon@gnu.ac.kr>
2025-08-21 11:51:42 +0900

    ✨ Feat[#21]: GroupRoutineListDoneCheck Entity 추가

---
+++ src/main/java/com/saeparam/HeyRoutine/domain/routine/entity/GroupRoutineListDoneCheck.java
@@ -0,0 +1,29 @@
+package com.saeparam.HeyRoutine.domain.routine.entity;
+
+import com.saeparam.HeyRoutine.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroupRoutineListDoneCheck {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "group_routine_list_done_check_id", updatable = false, unique = true, nullable = false)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_routine_list_id", nullable = false)
+    private GroupRoutineList groupRoutineList;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "done_check", nullable = false)
+    private boolean doneCheck;
+}
